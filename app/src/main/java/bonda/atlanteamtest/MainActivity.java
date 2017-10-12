6e41635b04f85f96def17225df4719071995993f
Wyacheslav Bondarenko <wdbondarenko@gmail.com>
2017-10-13 01:00:26 +0300

    Created project. First commit.

---
+++ app/src/main/java/bonda/atlanteamtest/MainActivity.java
@@ -0,0 +1,13 @@
+package bonda.atlanteamtest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
