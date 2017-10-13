a05f667413436e1e7ba4191dc969ad0ed3229d9b
Wyacheslav Bondarenko <wdbondarenko@gmail.com>
2017-10-13 13:24:26 +0300

    Implemented listing tabs.

--- app/src/main/java/bonda/atlanteamtest/MainActivity.java
+++ app/src/main/java/bonda/atlanteamtest/MainActivity.java
@@ -1,13 +1,47 @@
 package bonda.atlanteamtest;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
 
+import bonda.atlanteamtest.adapters.TabsViewPagerAdapter;
+import bonda.atlanteamtest.fragments.ContactsFragment;
+import bonda.atlanteamtest.fragments.ListCardFragment;
+
+/**
+ * Главная активность приложения
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Меню вкладок
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+
+        // Контейнер вкладок
+        ViewPager viewPager = (ViewPager) findViewById(R.id.view_pager);
+
+        // Инициализацая работы с контейнером
+        setupViewPager(viewPager);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+    /**
+     * Создание вкладок
+     */
+    private void setupViewPager(ViewPager viewPager) {
+        //Инициализация адаптера
+        TabsViewPagerAdapter adapter = new TabsViewPagerAdapter(this.getSupportFragmentManager());
+
+        // Добавление вкладок
+        adapter.addFragment(ListCardFragment.newInstance(), getString(R.string.name_tab_list_card));
+        adapter.addFragment(ContactsFragment.newInstance(), getString(R.string.name_tab_contacts));
+
+        // Инициализация работы с вкладками
+        viewPager.setAdapter(adapter);
     }
 }
